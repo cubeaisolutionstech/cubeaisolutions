4268b941b0651a48d283769e82ef46ff39e234c5
cubeaisolutionstech <165769704+cubeaisolutionstech@users.noreply.github.com>
2024-10-19 14:05:23 +0530

    Add files via upload

---
+++ Service Page/service.js
@@ -0,0 +1,20 @@
+
+
+function togglePopup(popupId, icon) {
+    const popup = document.getElementById(popupId);
+    const isActive = popup.style.display === "block";
+    popup.style.display = isActive ? "none" : "block";
+    icon.classList.toggle("active", !isActive);
+}
+
+document.addEventListener('click', function(event) {
+    const isClickOnIcon = event.target.closest('.menu-icon');
+    if (!isClickOnIcon) {
+        document.querySelectorAll('.popup, #header-popup').forEach(popup => {
+            popup.style.display = 'none';
+        });
+        document.querySelectorAll('.menu-icon').forEach(icon => {
+            icon.classList.remove('active');
+        });
+    }
+});
